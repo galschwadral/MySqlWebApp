448fc0862f0206c3c13e0a911931c07c8389d5cd
colin feghali <colin.feghali@gmail.com>
2019-12-16 11:18:36 +0100

    Connection funktioniert

--- MySqlWebApp/MySqlCon.cs
+++ MySqlWebApp/MySqlCon.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace MySqlWebApp
 {
@@ -15,6 +13,8 @@ namespace MySqlWebApp
         private string userId;
         private string password;
 
+        public string info = "not";
+
         public MySqlCon()
         {
             Initialize();
@@ -23,19 +23,19 @@ namespace MySqlWebApp
         private void Initialize()
         {
             server = "localhost";
-            database = "gebäudeManager";
+            database = "building-manager";
             userId = "root";
             password = "moi600";
 
-            string connectionString;
-            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
-            database + ";" + "USERID=" + userId + ";" + "PASSWORD" + 
-            password + ";";
+            string conTest = "Server=localhost;Database=building-manager;Uid=root;Pwd=moi600;";
+            string connectionString = "Server=" + server + ";" + "Database=" +
+            database + ";" + "Uid=" + userId + ";" + "Pwd=" +
+            password + ";"; 
 
             connection = new MySqlConnection(connectionString);
         }
 
-        private bool OpenConnection()
+        public bool OpenConnection()
         {
             try
             {
@@ -47,23 +47,23 @@ namespace MySqlWebApp
                 switch (ex.Number)
                 {
                     case 0:
-                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Cannot connect to Server" + "');", true);
+                        //ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Cannot connect to Server" + "');", true);
 
                         break;
 
                     case 1045:
-                        MessageBox.Show("Invalid username/password, please try again");
+                        //MessageBox.Show("Invalid username/password, please try again");
                         break;
 
                     default:
-                        MessageBox.Show(ex.Message);
+                        //MessageBox.Show(ex.Message);
                         break;
                 }
                 return false;
             }
         }
 
-        private bool CloseConnection()
+        public bool CloseConnection()
         {
             try
             {
@@ -72,8 +72,90 @@ namespace MySqlWebApp
             }
             catch (MySqlException ex)
             {
-                MessageBox.Show(ex.Message);
+                //MessageBox.Show(ex.Message);
                 return false;
+            }
+        }
+
+        public void Insert()
+        {
+            string querry = "INSERT INTO building (bName, bTotalspace, bLocation) VALUES ('Büro Gebäude Kreuzplatz', '470', '2')";
+            
+            if(this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(querry, connection);
+
+                cmd.ExecuteNonQuery();
+
+                info = "sexy";
+
+                //this.CloseConnection();
+            }
+        }
+
+        public void Update()
+        {
+            string querry = "UPDATE building SET bname='Büro Gebäude am Kreuzplatz', bTotalspace='550', bLocation='3'WHERE bName='Büro Gebäude Kreuzplatz'";
+            
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(querry, connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void Delete()
+        {
+            string querry = "DELETE FROM building WHERE bName='Büro Gebäude Kreuzplatz'";
+            
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(querry, connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void Select()
+        {
+            string query = "SELECT * FROM building";
+            this.OpenConnection();
+            DataTable table = new DataTable();
+
+            if (this.OpenConnection() == true)
+            {
+                
+
+                try
+                {
+                    
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                    dataAdapter.SelectCommand = new MySqlCommand(query, connection);
+                    
+                    dataAdapter.Fill(table);
+                    
+                }
+
+                finally
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        /*string ID = row["ColumnID"].ToString();
+                        string Name = row["columnName"].ToString();
+                        string FamilyName = row["ColumnFamilyName"].ToString();*/
+                    }
+                }
+                this.CloseConnection();
+              
+            }
+            else
+            {
+                
             }
         }
     }
---
+++ MySqlWebApp/MySqlObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlWebApp
+{
+    public class MySqlObject
+    {
+        int id { get; set; }
+        int location { get; set; }
+        string name { get; set; }
+
+
+    }
+}
